8710e130462f7d5893f843fb8aadb118219e24b9
aelshayeb <ali.livenet.el@gmail.com>
2019-08-13 21:17:54 -0400

    Add Unit Test for GetMove

---
+++ api/getMove.test.js
@@ -0,0 +1,63 @@
+const chai = require('chai');
+const expect = chai.expect;
+const url = 'http://localhost:8000';
+const request = require('supertest')(url);
+
+describe('Movement Api:', () => {
+  it('should return 400 error when position is lowercase', done => {
+    request
+      .post('/api/movement')
+      .send({ pos: 'a1' })
+      .expect(400)
+      .end((err, res) => {
+        if (err) return done(err);
+        done();
+      });
+  });
+
+  it('should return 400 error when position is not between A and H', done => {
+    request
+      .post('/api/movement')
+      .send({ pos: 'Z1' })
+      .expect(400)
+      .end((err, res) => {
+        if (err) return done(err);
+        done();
+      });
+  });
+
+  it('should return 400 error when position is null', done => {
+    request
+      .post('/api/movement')
+      .send({ pos: '' })
+      .expect(400)
+      .end((err, res) => {
+        if (err) return done(err);
+        done();
+      });
+  });
+
+  it('should return 400 error when position is not between 1 and 8', done => {
+    request
+      .post('/api/movement')
+      .send({ pos: 'a0' })
+      .expect(400)
+      .end((err, res) => {
+        if (err) return done(err);
+        done();
+      });
+  });
+
+  it('should return valid positions', done => {
+    request
+      .post('/api/movement')
+      .send({ pos: 'A4' })
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.data).to.be.an('array');
+        expect(res.body.data).to.deep.equal(['B6', 'B2', 'C5', 'C3']);
+        done();
+      });
+  });
+});
